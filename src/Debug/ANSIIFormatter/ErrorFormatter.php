b53b9084907b9c7f4ff5c649f25ce86de470a358
Gergely Laborci <gergely@laborci.hu>
2022-09-20 19:47:45 +0200

    wip

--- src/Debug/ANSIIFormatter/ErrorFormatter.php
+++ src/Debug/ANSIIFormatter/ErrorFormatter.php
@@ -8,14 +8,16 @@ class ErrorFormatter implements FormatterInterface {
 
 	public function __construct(private readonly string $root = "") {}
 
-	private function formatFile(string $file): string {
+	private function formatFile(string|null $file): string {
+		if(is_null($file)) return Color::LIGHT_YELLOW."?unknown?";
 		if (str_starts_with($file, $this->root)) $file = trim(substr($file, strlen($this->root)), '/');
 		$dir = Color::YELLOW . pathinfo($file, PATHINFO_DIRNAME);
 		$file = Color::LIGHT_YELLOW . pathinfo($file, PATHINFO_BASENAME);
 		return $dir . "/" . $file;
 	}
 
-	private function formatClass(string $class): string {
+	private function formatClass(string|null $class): string {
+		if(is_null($class)) return Color::LIGHT_CYAN_ALT."?unknown?";
 		$parts = explode("\\", $class);
 		$class = Color::LIGHT_CYAN_ALT . array_pop($parts);
 		$ns = Color::BLUE . join("\\", $parts) . "\\";
